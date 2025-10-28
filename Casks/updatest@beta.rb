82e4841cd62d97cb243bfc586e47630ba8ccc9a6
HugeIRL <38887481+HugeIRL@users.noreply.github.com>
2025-10-28 10:49:25 -0500

    add version build

--- Casks/updatest@beta.rb
+++ Casks/updatest@beta.rb
@@ -1,5 +1,5 @@
 cask "updatest@beta" do
-  version "1.0.0.BETA,"
+  version "1.0.0.BETA,1312"
   sha256 "7566d3a1dbb626dfafa3e3ff2587bba1499484e61702d64469d4c88bbb5b0a1c"
 
   url "https://updatest.app/download/Updatest.zip"
