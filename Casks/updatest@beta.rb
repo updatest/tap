97bac5b36f5a4a12b071f225bcf48bb3877dc986
HugeIRL <38887481+HugeIRL@users.noreply.github.com>
2025-10-28 10:46:55 -0500

    add cask

---
+++ Casks/updatest@beta.rb
@@ -0,0 +1,25 @@
+cask "updatest@beta" do
+  version "1.0.0.BETA,"
+  sha256 "7566d3a1dbb626dfafa3e3ff2587bba1499484e61702d64469d4c88bbb5b0a1c"
+
+  url "https://updatest.app/download/Updatest.zip"
+  name "Updatest"
+  desc "All in one app update tool for macOS including Homebrew cask management"
+  homepage "https://updatest.app"
+
+  livecheck do
+    url "https://updatest.app/download/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+
+  app "Updatest.app"
+
+  zap trash: [
+    "~/Library/Application Support/Updatest",
+    "~/Library/Caches/app.updatest.Updatest",
+    "~/Library/Preferences/app.updatest.Updatest.plist",
+    "~/Library/Saved Application State/app.updatest.Updatest.savedState"
+  ]
+end
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# tap
+# Updatest Tap
+
+Homebrew tap for Updatest BETA releases.
+
+https://updatest.app for more information
+
+## Installation
+
+```bash
+brew tap updatest/tap https://github.com/updatest/tap.git
+brew install --cask updatest@beta
+```
