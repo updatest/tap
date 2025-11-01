9e6563de402c2b8d0ff3b287c26438bdb3306dbf
Jared Mariash <38887481+HugeIRL@users.noreply.github.com>
2025-11-01 17:13:08 -0500

    Update version and SHA256 for updatest@beta

--- Casks/updatest@beta.rb
+++ Casks/updatest@beta.rb
@@ -1,6 +1,6 @@
 cask "updatest@beta" do
-  version "1.0.0.BETA,1489"
-  sha256 "3bd7c4ade59de082b58f33e37a284e09f7a2a5245ad414bf0a33e7a45b4e69ac"
+  version "1.0.0.BETA,1497"
+  sha256 "4ce7f7882056ba7d22c4c4c5c6844af10fc265cc7e91857c4718ba4b3e0c75a8"
 
   url "https://updatest.app/download/Updatest.zip"
   name "Updatest"
